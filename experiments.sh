a38b99ef4d01c53f5ecad72d0c9c587b219c6b42
James Lin <b04901069@ntu.edu.tw>
2019-01-28 20:56:11 +0800

    add exp

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,4 @@
 #python3 main.py train base_01 -batch_size 8 -epoch_num 100 -save ../weights/bese_01_01.pkl -log logs/base_01_01.txt -check_batch_num 20
 python3 main.py train fcn16s_01 -batch_size 6 -epoch_num 50 -save ../weights/fcn16s_01_01.pkl -log logs/fcn16s_01_01.txt -check_batch_num 20
-
+#python3 main.py train fcn16s_02 -batch_size 6 -epoch_num 50 -save ../weights/fcn16s_02_01.pkl -log logs/fcn16s_02_01.txt -check_batch_num 20
+#python3 main.py train fcn8s     -batch_size 6 -epoch_num 50 -save ../weights/fcn8s_01.pkl     -log logs/fcn8s.txt        -check_batch_num 20

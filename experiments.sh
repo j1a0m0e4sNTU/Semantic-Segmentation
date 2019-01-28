af1d3c7a951f36580c8fed1209f95d9b6d1abb41
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-01-28 11:29:38 +0000

    edit exp

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #python3 main.py train base_01 -batch_size 8 -epoch_num 100 -save ../weights/bese_01_01.pkl -log logs/base_01_01.txt -check_batch_num 20
-python3 main.py train fcn16s_01 -batch_size 10 -epoch_num 50 -save ../weights/bese_01_01.pkl -log logs/fcn16s_01_01.txt -check_batch_num 20
+python3 main.py train fcn16s_01 -batch_size 6 -epoch_num 50 -save ../weights/bese_01_01.pkl -log logs/fcn16s_01_01.txt -check_batch_num 20
 
